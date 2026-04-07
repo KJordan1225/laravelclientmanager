c9f5f51f5bad5fcd77ac4af9b89b791cf3f23949
Keith Jordan <shadow902@gmail.com>
2026-04-07 17:00:15 -0400

    CHUNK 4

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,27 @@
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-import App from './App.vue';
-import router from './router';
-import axios from 'axios';
+import '../css/app.css';
+import './bootstrap';
 
-axios.defaults.baseURL = '/api';
-axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createApp, h } from 'vue';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
-const app = createApp(App);
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
-app.config.globalProperties.$http = axios;
-
-app.use(createPinia());
-app.use(router);
-app.mount('#app');
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob('./Pages/**/*.vue'),
+        ),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
